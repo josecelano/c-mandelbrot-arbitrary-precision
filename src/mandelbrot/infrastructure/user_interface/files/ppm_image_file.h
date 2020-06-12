d3387ccb940d9c00e882ff373242b4ccf1685817
Jose Celano <jose.celano@hyve.net>
2020-06-12 08:53:54 +0100

    Refactor: private function

--- src/mandelbrot/infrastructure/user_interface/files/ppm_image_file.h
+++ src/mandelbrot/infrastructure/user_interface/files/ppm_image_file.h
@@ -5,8 +5,6 @@
 #include "../../../domain/fractal_data.h"
 #include "../../../domain/image/color_map.h"
 
-void render_and_write_out_image(char *filename, fractal_data_t fractal_data, color_map_t color_map);
-
 void render_ppm_image(fractal_data_t fractal_data, color_map_t color_map);
 
 #endif //C_MANDELBROT_ARBITRARY_PRECISION_PPM_IMAGE_FILE_H
